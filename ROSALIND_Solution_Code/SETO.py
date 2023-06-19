005dc2d5137c95085a90ed5c7b5410fb0e1a21d2
stunme <33939211+stunme@users.noreply.github.com>
2023-06-19 14:14:17 -0700

    commit

--- ROSALIND_Solution_Code/SETO.py
+++ ROSALIND_Solution_Code/SETO.py
@@ -1,9 +1,10 @@
 ##  Introduction to Set Operations
 
-n = 10
-setC = set(i for i in range(1,n+1))
-set1 ={1, 2, 3, 4, 5}
-set2 = {2, 8, 5, 10}
+with open("test.txt","r") as f:
+    n = int(f.readline().strip())
+    setC = set(i for i in range(1,n+1))
+    set1 = set(int(i) for i in f.readline().strip().strip(" {}").split(","))
+    set2 = set(int(i) for i in f.readline().strip().strip(" {}").split(","))
 
 
 print(set1|set2)

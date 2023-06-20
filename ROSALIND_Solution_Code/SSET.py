f4e568d549d0004c95a2720f36314d572b717336
stunme <yangye.chn@gmail.com>
2023-06-19 19:01:19 -0700

    commit

--- ROSALIND_Solution_Code/SSET.py
+++ ROSALIND_Solution_Code/SSET.py
@@ -1,35 +1,13 @@
-##  Counting Subsets
+##  Counting Subsets 
+##  counting subset of n set is 2^n
 
-import math 
+n = 3
+mod = 1000000
 
-def factorial(m,n,mod = 1000000):
-    result = 1
-    for i in range(m,m-n,-1):
-        if i ==0:
-            break
-        result *= i
-        if result>mod:
-            result %= mod
-    return result
+total = 1
+for n in range(1,n+1):
+    total*=2
+    if total>mod:
+        total %= mod
 
-def subset(n, mod=1000000):
-    total = 1
-    for i in range(1,(n-1)//2+1):
-        total += factorial(n,i,mod)/factorial(i,i,mod)
-        if total>mod:
-            result %=mod
-
-    if n%2:
-        return int(2*total%mod)
-    else:
-        i = n//2
-        return int(2*total+factorial(n,i,mod)/factorial(i,i,mod)%mod)
-        
-#print(factorial(424,424))
-#print(subset(848))
-#for i in range(5,1,-1):
-    # print(i)
-
-
-for i in range(1000):
-    print(i,factorial(i,i))
+print total

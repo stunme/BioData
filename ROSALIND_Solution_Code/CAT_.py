74501aa2371b974f018b4631c447a0f31ca080ce
stunme <yangye.chn@gmail.com>
2023-06-21 19:59:19 -0700

    commit

--- ROSALIND_Solution_Code/CAT_.py
+++ ROSALIND_Solution_Code/CAT_.py
@@ -1,28 +1,57 @@
 ##  Catalan Numbers and RNA Secondary Structures
 
-def validatePerfect(seq):
-    if not len(seq)%2:
-        dic = {"A":0,
-               "U":0,
-               "G":0,
-               "C":0
-               }
-        for i in seq:
-            dic[i] += 1
-        if dic["A"] == dic["U"] and dic["G"] == dic["C"]:
-            return False
-    return True
+from utility import readFastaFileList
 
-def catalan(seq, mod = 1000000):
-    
-    if validatePerfect(seq):
+dicAll={}
+dicMatch = {'A':'U',
+            'U':'A',
+            'G':'C',
+            'C':'G'
+            }
+
+def catalan(start, end, mod = 1000000):
+    s = seq[start:end]
+    if s.count('A')!=s.count('U') or s.count('G')!=s.count('C'):
         return 0
-    if len(seq)==2:
+    if end <= start + 2:
         return 1
-    sum = catalan(seq[1:-1])%mod+catalan(seq[2:])%mod
-    for i in range(3,len(seq)-1,2):
-        sum += (catalan(seq[1:i-1])*catalan(seq[i+1:]))%mod
+    sum =0
+    for i in range(start+1,end,2):
+        if dicMatch[seq[start]] == seq[i]:
+            left = (start+1,i)
+            right = (i+1,end)
+            if left not in dicAll:
+                dicAll[left] =catalan(left[0],left[1])
+            if right not in dicAll:
+                dicAll[right] = catalan(right[0],right[1])
+            sum += dicAll[left]*dicAll[right]%mod
+            sum %= mod
     return sum%mod
 
-seq = "UAGCGUGAUCAC"
-print catalan(seq)
+
+seq = readFastaFileList("test.txt")[0]
+
+import time
+start = time.time()
+print(catalan(0,len(seq)))
+print(time.time()-start)
+
+## =================================================================================================
+## a short solution from Rosalind. about 4 times slower.
+
+
+c = {'':1, 'A':0, 'C':0, 'G':0, 'U':0, 'AA':0, 'AC':0, 'AG':0, 'AU':1, 'CA':0, 'CC':0, 
+    'CG':1, 'CU':0, 'GA':0, 'GC':1, 'GG':0, 'GU':0, 'UA':1, 'UC':0, 'UG':0, 'UU':0}
+s=seq
+
+def catalan1(s, mod= 1000000):
+    if s not in c:
+        if s.count('C') != s.count('G') or s.count('A') != s.count('U'):
+            c[s] = 0
+        else:
+            c[s] = sum([catalan1(s[1:k]) * c[s[0]+s[k]] * catalan1(s[k+1:]) for k in range(1, len(s), 2)])
+    return c[s]%mod
+
+start = time.time()
+print(catalan1(s))
+print(time.time()-start)

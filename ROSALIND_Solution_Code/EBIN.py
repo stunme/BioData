4f4473c2935f2da31ac7cf57377654728cbbaadb
stunme <33939211+stunme@users.noreply.github.com>
2023-06-23 16:03:22 -0700

    commit

---
+++ ROSALIND_Solution_Code/EBIN.py
@@ -0,0 +1,7 @@
+##  Wright-Fisher's Expected Behavior
+
+with open("test.txt","r") as f:
+    n = int(f.readline().strip())
+    P = map(float, f.readline().strip().split(" "))
+
+print(" ".join(str(n*i) for i in P))

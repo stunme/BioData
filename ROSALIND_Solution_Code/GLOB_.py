b0e1028d79b7032f8376813005d137e6457c9c48
stunme <33939211+stunme@users.noreply.github.com>
2023-06-30 11:42:44 -0700

    commit

---
+++ ROSALIND_Solution_Code/GLOB_.py
@@ -0,0 +1,36 @@
+##  Global Alignment with Scoring Matrix
+
+
+with open("BLOSUM62.txt","r") as f:
+    aa = f.readline().strip().split()
+    m = [l.strip().split() for l in f.readlines()]
+
+BLOSUM62 = {}
+for i in range(len(aa)):
+   for j in range(1,len(aa)+1):
+      BLOSUM62[aa[i]+aa[j-1]] = int(m[i][j])
+  
+def glob(seq1, seq2, scoreMatrix):
+   gap= -5
+   lenI, lenJ = len(seq1)+1, len(seq2)+1
+   arr = []
+   for i in range(lenI):
+      arr.append([0]*lenJ)
+   for j in range(1, lenJ):
+      arr[0][j] = gap*j
+   for i in range(1, lenI):
+      arr[i][0] = gap*i
+
+   for i in range(1,lenI):
+      for j in range(1, lenJ):
+         arr[i][j] = max(
+                           arr[i][j-1]+gap,
+                           arr[i-1][j]+gap,
+                           arr[i-1][j-1]+scoreMatrix[seq1[i-1]+seq2[j-1]]
+               )
+            
+   return arr[i][j]
+
+seq1 = "PLEASANTLY"
+seq2 = "MEANLY"
+print(glob(seq1, seq2, BLOSUM62))

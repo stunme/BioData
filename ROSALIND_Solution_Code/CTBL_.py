4376d2417ee414062c8e62832cb755a40c6b0cc0
stunme <yangye.chn@gmail.com>
2023-07-06 09:06:27 -0700

    commit

--- ROSALIND_Solution_Code/CTBL_.py
+++ ROSALIND_Solution_Code/CTBL_.py
@@ -26,40 +26,7 @@ def ctbl(treeStr):
                 charTable[i].append('1')
     print(charTable)            
 
-        # if t == '(':
-        #     stack += [[newTwoPos]]
-        # elif t not in ',()':
-        #     stack[-1].append(t)
-        # elif t == ')':
-        #     cur = stack.pop()
-        #     print(cur[0],newTwoPos)
-            
-        #     if cur[0]+1 < len(charTable) and stack != [[1]] and cur[0]+2 !=newTwoPos and cur[0]!=newTwoPos:
-        #         for i in range(cur[0]):
-        #                 charTable[i].append('0')
-        #         for i in range(cur[0],len(charTable)):
-        #             charTable[i].append('1')
-
-        #     if len(cur)<2:
-        #         continue
-        #     elif len(cur)>2:
-        #         for i in charTable:
-        #             i.append('0')
-        #         for i in range(1,len(cur)):
-        #             charTable += [['0']*len(charTable[-1])]
-        #             charTable[-1][0] = cur[i]
-        #             charTable[-1][-1] = '1'
-        #         newTwoPos = len(charTable)
-        #     else:    
-        #         for i in range(1,len(cur)):
-        #             charTable += [['0']*len(charTable[-1])]
-        #             charTable[-1][0] = cur[i]
-        #         for i in range(cur[0]):
-        #             charTable[i].append('0')
-        #         for i in range(cur[0],len(charTable)):
-        #             charTable[i].append('1')
-                
-        # print(charTable)
+       
             
     
     # print(num,num0,num2)

df6d31aa8a5684fda26b8f536b4dab3e2208a685
stunme <yangye.chn@gmail.com>
2023-07-06 07:59:49 -0700

    commit

--- ROSALIND_Solution_Code/CTBL_.py
+++ ROSALIND_Solution_Code/CTBL_.py
@@ -5,41 +5,59 @@ def ctbl(treeStr):
     tokens = re.split('([(),])',treeStr)
     charTable = [['1']]
     stack = [[1]]
-    newTwoPos = 1
+    curLeft = 1
+    # newTwoPos = 1
 
     for t in tokens:
         if t == '(':
-            stack += [[newTwoPos]]
+            stack += [[curLeft]]
         elif t not in ',()':
             stack[-1].append(t)
         elif t == ')':
             cur = stack.pop()
-            print(cur[0],newTwoPos)
-            
-            if cur[0]+1 < len(charTable) and stack != [[1]] and cur[0]+2 !=newTwoPos and cur[0]!=newTwoPos:
-                for i in range(cur[0]):
-                        charTable[i].append('0')
-                for i in range(cur[0],len(charTable)):
-                    charTable[i].append('1')
+            left,cur = cur[0], cur[1:]
+            for i in range(len(cur)):
+                charTable += [['0']*len(charTable[-1])]
+                charTable[-1][0] = cur[i]
+            curLeft += len(cur)
+            for i in range(left):
+                charTable[i].append('0')
+            for i in range(left,curLeft):
+                charTable[i].append('1')
+    print(charTable)            
 
-            if len(cur)<2:
-                continue
-            elif len(cur)>2:
-                for i in charTable:
-                    i.append('0')
-                for i in range(1,len(cur)):
-                    charTable += [['0']*len(charTable[-1])]
-                    charTable[-1][0] = cur[i]
-                    charTable[-1][-1] = '1'
-                newTwoPos = len(charTable)
-            else:    
-                for i in range(1,len(cur)):
-                    charTable += [['0']*len(charTable[-1])]
-                    charTable[-1][0] = cur[i]
-                for i in range(cur[0]):
-                    charTable[i].append('0')
-                for i in range(cur[0],len(charTable)):
-                    charTable[i].append('1')
+        # if t == '(':
+        #     stack += [[newTwoPos]]
+        # elif t not in ',()':
+        #     stack[-1].append(t)
+        # elif t == ')':
+        #     cur = stack.pop()
+        #     print(cur[0],newTwoPos)
+            
+        #     if cur[0]+1 < len(charTable) and stack != [[1]] and cur[0]+2 !=newTwoPos and cur[0]!=newTwoPos:
+        #         for i in range(cur[0]):
+        #                 charTable[i].append('0')
+        #         for i in range(cur[0],len(charTable)):
+        #             charTable[i].append('1')
+
+        #     if len(cur)<2:
+        #         continue
+        #     elif len(cur)>2:
+        #         for i in charTable:
+        #             i.append('0')
+        #         for i in range(1,len(cur)):
+        #             charTable += [['0']*len(charTable[-1])]
+        #             charTable[-1][0] = cur[i]
+        #             charTable[-1][-1] = '1'
+        #         newTwoPos = len(charTable)
+        #     else:    
+        #         for i in range(1,len(cur)):
+        #             charTable += [['0']*len(charTable[-1])]
+        #             charTable[-1][0] = cur[i]
+        #         for i in range(cur[0]):
+        #             charTable[i].append('0')
+        #         for i in range(cur[0],len(charTable)):
+        #             charTable[i].append('1')
                 
         # print(charTable)
             
@@ -59,3 +77,18 @@ with open("result.txt",'w') as f:
     for i in range(1,len(ctbl_[0])):
         f.write(" ".join((j[i]) for j in ctbl_))
         f.write("\n")
+
+
+
+with open("result.txt","r") as f:
+   a, b= [],[]
+   for l in f.readlines():
+      a.append(l.strip())
+      b.append(l.strip().replace('1','7').replace('0','1').replace('7','0'))
+
+
+for x in range(len(a)-1,-1,-1):
+   for y in range(len(b)-1,x-1,-1):
+      if a[x] == b[y]:
+        print(len(a), len(b), y+1)
+print(len(a),len(a[0]))

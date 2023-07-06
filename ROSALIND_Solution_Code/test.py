57b0dc016b43015707781b67409328db18ff1d15
stunme <yangye.chn@gmail.com>
2023-07-06 16:27:22 -0700

    commit

--- ROSALIND_Solution_Code/test.py
+++ ROSALIND_Solution_Code/test.py
@@ -1,3 +1,11 @@
-print("Hello the world")
-while True:
-    print("non-stop")
+with open("test.txt",'r') as f:
+    page = f.read()
+
+import re
+for i in re.split('{"|,"',page):
+    cur = i.replace('"','').split(':')
+    if cur[0] =="name":
+        
+        print(cur[1])
+
+

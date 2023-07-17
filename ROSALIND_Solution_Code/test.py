5ecccf9919c58da304b42842247fd2e852bfcec6
stunme <yangye.chn@gmail.com>
2023-07-17 00:05:50 -0700

    commit

--- ROSALIND_Solution_Code/test.py
+++ ROSALIND_Solution_Code/test.py
@@ -1,3 +1,18 @@
-test = {'a':1,"b":2}
+import itertools as it
 
-print('a' not in test)
+with open("result.txt",'r') as f:
+    seqList = [[]]
+    for l in f.readlines():
+        if "=" not in l:
+            seqList[-1].append(l.strip())
+        else:
+            seqList.append([])
+
+for sl in seqList:
+
+    count = 0
+    for a,b in it.combinations(sl[-2:],2):
+        for x,y in zip(a,b):
+            if x!=y:
+                count -= 1
+    print(count)

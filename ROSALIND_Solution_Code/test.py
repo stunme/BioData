355d006a61f7920917fec00ff5a2c05384c5ebf4
stunme <yangye.chn@gmail.com>
2023-07-17 07:58:11 -0700

    commit

--- ROSALIND_Solution_Code/test.py
+++ ROSALIND_Solution_Code/test.py
@@ -1,46 +1,13 @@
 import itertools as it
-taxas = "ABCDEF"
+import random
+from collections import Counter
 
-set1 = "1234567899128391748772783560891740921473312356"
+with open("test.txt",'r') as f:
+    charListTable = [i.strip() for i in f.readlines()]
 
-checker = set()
-for i in range(1,len(taxas)):
-    for x in it.combinations(taxas[1:],i):
-        tmp1 = [m for m in taxas if m not in x]
-        for j in range(1,len(tmp1)):
-            for y in it.combinations(tmp1[1:],j):
-                tmp2 = tuple([m for m in tmp1 if m not in y])
-                ct = [""]*6
-                for m in range(len(taxas)):
-                    if taxas[m] in tmp2:
-                        ct[0] += "1"
-                        ct[1] += "0"
-                    else:
-                        ct[0] += "0"
-                        ct[1] += "1"
-                for m in range(len(taxas)):
-                    if taxas[m] in y:
-                        ct[2] += "1"
-                        ct[3] += "0"
-                    else:
-                        ct[2] += "0"
-                        ct[3] += "1"
-                for m in range(len(taxas)):
-                    if taxas[m] in x:
-                        ct[4] += "1"
-                        ct[5] += "0"
-                    else:
-                        ct[4] += "0"
-                        ct[5] += "1"
-                noFind = True
-                for m in ct:
-                    if m.count("0")<2 or m.count("1")<2:
-                        continue
-                    elif m in checker:
-                        noFind = False
-                        break
-                if noFind:
-                    for m in ct:
-                        checker.add(m)
-                    print(x,tmp2,y)
-                        # print(tmp2,xy,xt,yt,"===========")
+a = random.sample(charListTable,3)
+x = Counter(zip(a[0],a[1],a[2]))
+print(x)
+
+
+for i in a:

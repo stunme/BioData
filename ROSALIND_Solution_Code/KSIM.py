393b07c029d1c01fd50f45ecf044c8108c1f0480
stunme <yangye.chn@gmail.com>
2023-07-19 20:12:17 -0700

    commit

--- ROSALIND_Solution_Code/KSIM.py
+++ ROSALIND_Solution_Code/KSIM.py
@@ -4,6 +4,59 @@ from utility import readFastaFileList
 
 def ksim(seqI, seqJ, k):
     k += 1
+    lenI, lenJ = len(seqI)+1, len(seqJ)+1
+    aligns = set()
+    cur = [j for j in range(lenJ)]
+    curPos = [{j} for j in range(lenJ)]
+    for i in range(1,lenI):
+        pre = cur.copy()
+        prePos = curPos
+        curPos = [{i}]
+        for j in range(1,lenJ):
+            if seqI[i-1]==seqJ[j-1]:
+                cur[j] = pre[j-1]
+                curPos.append(prePos[j-1].copy())
+            else:
+                a = pre[j-1] +1
+                b = pre[j]+1
+                c = cur[j-1]+1
+                cur[j] = min(a,b,c)
+                curPos.append(set())
+                if cur[j]<k:
+                    if cur[j] == a:
+                        curPos[j] |= prePos[j-1]
+                    if cur[j] == b:
+                        curPos[j] |= prePos[j]
+                    if cur[j] == c:
+                        curPos[j] |= curPos[j-1]
+        # if cur[j]<k:
+        for m in curPos[j]:
+            aligns.add((m+1,i-m))
+            for n in range(1,k-cur[j]):
+                aligns.add((m+1+n,i-m-n))
+                aligns.add((max(1,m+1-n),i-m+n))
+    return aligns
+
+import time
+start = time.time()
+with open("test.txt",'r') as f:
+    k = int(f.readline().strip())
+    motif = f.readline().strip()
+    genome = f.readline().strip()
+    
+with open("result.txt",'w') as f:
+    for i in ksim(genome,motif,k):
+        f.write(' '.join(str(j) for j in i))
+        f.write('\n')
+print(time.time()-start)
+
+
+#Use more time, but works for all sample
+
+
+# 2x faster and works for k > 3
+def ksim_(seqI, seqJ, k):
+    k += 1
     lenI = len(seqI)+1
     lenJ = len(seqJ)+1
     aligns = []
@@ -31,7 +84,7 @@ def ksim(seqI, seqJ, k):
                 cur[j] +=1
         if cur[j]<k:
             aligns.append((i,cur[j]))
-    result = []
+    result = set()
     for a,b in aligns:
         i = a
         j = lenJ-1
@@ -43,18 +96,14 @@ def ksim(seqI, seqJ, k):
                 i -=1
             elif tracker[i][j] == 2:
                 j -=1
-        result.append([i+1,a-i])
+        result.add((i+1,a-i))
         for m in range(1,k-b):
-                result.append([i+1+m,a-i-m])
-                result.append([i+1-m,a-i+m])
+                result.add((i+1+m,a-i-m))
+                result.add((max(1,i+1-m),a-i+m))
     return result
 
-with open("test.txt",'r') as f:
-    k = int(f.readline().strip())
-    motif = f.readline().strip()
-    genome = f.readline().strip()
-with open("result.txt",'w') as f:
-    for i in ksim(genome,motif,k):
-        f.write(' '.join(str(j) for j in i))
-        f.write('\n')
 
+# with open("result.txt",'w') as f:
+#     for i in ksim_(genome,motif,k):
+#         f.write(' '.join(str(j) for j in i))
+#         f.write('\n')
